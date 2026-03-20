25df135e4c658162e41bebc0e1f834cb1f20fd4f
Duvan Martinez <duvanjmb@gmail.com>
2026-03-20 15:12:44 -0500

    vite authorization change

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './'
+  base: './',
+  preview: {
+    allowedHosts: true, // 👈 Esto permite cualquier host en modo preview (ideal para despliegue)
+    port: 80,           // Aseguramos que use el puerto 80 que pide SeeNode
+    host: '0.0.0.0'     // Aseguramos que escuche en todas las interfaces
+  }
 })
